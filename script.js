bde702bcbf55856d70d9c7e062af48b12d5ce98e
lahsouza123 <126804015+lahsouza123@users.noreply.github.com>
2024-06-19 13:37:20 -0300

    js mudanças

--- script.js
+++ script.js
@@ -27,3 +27,23 @@ const perguntas = [
         ]
     }
 ];
+
+let atual = 0; 
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas(); 
+} 
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
+
