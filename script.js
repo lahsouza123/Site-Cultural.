b163b3caa46a5df56fb741927e2b9bbb4ffe24e0
lahsouza123 <126804015+lahsouza123@users.noreply.github.com>
2024-07-24 14:01:00 -0300

    alteracao

--- script.js
+++ script.js
@@ -7,23 +7,42 @@ const textoResultado = document.querySelector('.texto-resultado');
 const perguntas = [
     {
         enunciado: "Qual festa cultural vocẽ prefere?",
-        alternativas: [
-            "Festa junina",
-            "Folia de Reis"
+        alternativas: [ 
+            {
+                texto: "Festa junina" ,
+                afirmacao: "Comemorada durante todo o mês de junho, a festa junina é a mais típica, comemorada em todo o Brasil, conta com danças, comidas típicas, entre outros."
+            },
+            {
+                texto:"Folia de Reis",
+                afirmacao: "Relembrando a visita dos reis magos ao menino Jesus, a festa engloba músicas, orações, danças, comidas típicas, entre outros."
+            },
+
         ]
     },
     {
         enunciado: "Vocẽ gosta de dançar quadrilha ou reisado?",
         alternativas: [
-            "Quadrilha",
-            "Reisado"
+            {
+                texto: "Quadrilha",
+                afirmacao: "Trata-se de uma dança coletiva em pares, que possui uma coreogrfia específica baseada em passos tradicionais e um orador, que proclama frases divertidas e determinam os movimentos da dança."
+            },
+            {
+                texto: "Reisado",
+                afirmacao: "Formado por um cortejo composto por dois cordões, em comemoração ao nascimento do menino Jesus e em homenagem dos reis magos, tendo como personagens principais o rei e a rainha."
+            }
         ]
     },
     {
         enunciado: "Que tal conhecer algumas comidas das festas?",
         alternativas: [
-            "Junina",
-            "Folia de Reis"
+            {
+                texto:"Festa junina",
+                afirmacao: "Comidas derivadas de milho e amendoim, bolos, maçã do amor, cachorro-quente, arroz doce, quentão, canjica, entre outros."
+            },
+            {
+                texto:"Folia de Reis",
+                afirmacao: "Costuma-se encontrar arroz, feijão, macarronada, carnes e verduras."
+            }
         ]
     }
 ];
@@ -40,7 +59,11 @@ function mostraPergunta() {
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++;
+            mostraPergunta();
+        })
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
