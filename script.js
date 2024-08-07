7c782204472aad29e60ab059bb690cb6682477f3
lahsouza123 <126804015+lahsouza123@users.noreply.github.com>
2024-08-07 12:54:28 -0300

    alteraçao

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ const perguntas = [
                 texto:"Folia de Reis",
                 afirmacao: "Relembrando a visita dos reis magos ao menino Jesus, a festa engloba músicas, orações, danças, comidas típicas, entre outros."
             },
-
         ]
     },
     {
@@ -47,25 +46,42 @@ const perguntas = [
     }
 ];
 
-let atual = 0; 
+
+let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
-    mostraAlternativas(); 
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
 }
 
 function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
+    for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", function(){
-            atual++;
-            mostraPergunta();
-        })
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
